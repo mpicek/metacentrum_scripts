3223e7c85c502812d9e2b0766fc0847f6180d11e
Martin Picek <martinpicek4@gmail.com>
2021-04-10 07:50:18 +0200

    Working to_be_run

--- to_be_run.sh
+++ to_be_run.sh
@@ -5,4 +5,5 @@ set -ueo pipefail
 cd labs/05
 python3 cags_classification.py --epochs=2 --epochs2=2 --threads=8
 
-cp cifar_competition_test.txt $DATADIR
+DATADIR="/storage/budejovice1/home/$(whoami)"
+cp cags_classification.txt $DATADIR
