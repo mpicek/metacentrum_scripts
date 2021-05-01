488d7f312ba7a1c6ea462de94ce93e96f69d8fb7
mpicek <martinpicek4@gmail.com>
2021-05-01 16:51:02 +0200

    Auto commit

--- qsub_script.sh
+++ qsub_script.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-
 # VARIABLES PASSED TO THIS SCRIPT
 #   - SEED ... seed to work with
 #   - CMD  ... command to be run
 
 # DEFINE RESOURCES:
 #PBS -N qsub_script
-#PBS -l select=1:ncpus=12:ngpus=1:mem=16gb:scratch_local=15gb:cuda_version=11.2:gpu_cap=cuda61
+#PBS -l select=1:ncpus=12:ngpus=1:mem=16gb:scratch_local=15gb:cuda_version=11.2:gpu_cap=cuda75
 #PBS -q gpu
 #PBS -l walltime=6:00:00
 
@@ -43,7 +42,7 @@ cd "$PROGRAM_PATH"
 cat out
 
 # if I want to copy the created model
-if [ "$ENSEMBLE" -eq "1"]; then
+if [ "$ENSEMBLE" -eq "1" ]; then
     cp -r submodel$SEED $DATADIR/models/submodel$SEED
 fi
 
